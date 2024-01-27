0a544e11767fc14705fa1e493cb2bd0d0ee0a6ad
idleCat90 <s.a.revin@yandex.ru>
2024-01-27 16:01:57 +0300

    feat: add User model, dtos and mapper

---
+++ main-service/src/main/java/ru/practicum/ewm/model/User.java
@@ -0,0 +1,23 @@
+package ru.practicum.ewm.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity(name = "user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String email;
+}

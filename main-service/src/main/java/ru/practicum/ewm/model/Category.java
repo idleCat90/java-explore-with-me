f4e971d728cec7b775dc1ea692c2d20123117dce
idleCat90 <s.a.revin@yandex.ru>
2024-01-27 18:05:36 +0300

    feat: add Category model, dto and mapper

---
+++ main-service/src/main/java/ru/practicum/ewm/model/Category.java
@@ -0,0 +1,21 @@
+package ru.practicum.ewm.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity(name = "category")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+}

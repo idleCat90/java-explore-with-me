165449cdf36f860a4954311960fe76ecf3f4e871
idleCat90 <s.a.revin@yandex.ru>
2024-02-07 23:06:05 +0300

    fix: remove unused imports

--- main-service/src/main/java/ru/practicum/ewm/dto/SearchEventParamsAdmin.java
+++ main-service/src/main/java/ru/practicum/ewm/dto/SearchEventParamsAdmin.java
@@ -1,6 +1,5 @@
 package ru.practicum.ewm.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 import ru.practicum.ewm.Constants;
